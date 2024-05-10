d0e8d0f23f398378ba165ff7ec39b466b3cc6d68
Gabriel Lucas <gabriellucasseverino@hotmail.com>
2024-05-10 08:14:11 -0300

    Feat: add productSpecs page

---
+++ src/modules/ProductSpecs/page/index.tsx
@@ -0,0 +1,55 @@
+import { useParams } from "react-router-dom";
+
+import { useSelector } from "react-redux";
+import { RootState } from "../../../store";
+import { addProductToCart } from "../../../store/reducers/cartReducer";
+import { useDispatch } from "react-redux";
+import {
+  AddToCartButton,
+  ProductContainer,
+  ProductDescription,
+  ProductDetails,
+  ProductImage,
+  ProductPrice,
+  ProductTitle,
+} from "./styles";
+import { Product } from "../../../service/products/payload/productPayload";
+
+const ProductDetail = () => {
+  const { id } = useParams();
+  const dispatch = useDispatch();
+  const cartItems = useSelector((state: RootState) => state.cart.allProduct);
+  const fildItem = cartItems.filter((item) => item.id === Number(id));
+
+  const handleAddToCart = (product: Product) => {
+    dispatch(addProductToCart(product));
+  };
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      {fildItem.map((items) => (
+        <ProductContainer key={items.id}>
+          <ProductImage src={items.photo} alt={items.name} />
+          <ProductDetails>
+            <ProductTitle>{items.name}</ProductTitle>
+            <ProductDescription>{items.description}</ProductDescription>
+            <ProductPrice>
+              Preço: R${parseFloat(items.price).toFixed(2)}
+            </ProductPrice>
+            <AddToCartButton onClick={() => handleAddToCart(items)}>
+              Adicionar ao Carrinho
+            </AddToCartButton>
+          </ProductDetails>
+        </ProductContainer>
+      ))}
+    </div>
+  );
+};
+
+export default ProductDetail;
---
+++ src/modules/ProductSpecs/page/styles.ts
@@ -0,0 +1,58 @@
+import styled from "styled-components";
+export const ProductContainer = styled.div`
+  display: flex;
+  align-items: start;
+  flex-direction: row;
+  margin: 20px;
+  padding: 20px;
+  border: 1px solid #ccc;
+  border-radius: 10px;
+  width: 100%;
+  max-width: 800px;
+`;
+
+export const ProductDetails = styled.div`
+  display: flex;
+  justify-content: center;
+
+  text-align: center;
+  gap: 15px;
+  flex-direction: column;
+  margin-left: 20px;
+`;
+
+export const ProductTitle = styled.h1`
+  color: #333;
+`;
+
+export const ProductImage = styled.img`
+  width: auto;
+  height: 300px;
+  object-fit: cover;
+  flex-shrink: 0;
+`;
+
+export const ProductDescription = styled.p`
+  color: #666;
+`;
+
+export const ProductPrice = styled.p`
+  font-size: 18px;
+  font-weight: bold;
+  color: #000;
+`;
+
+export const AddToCartButton = styled.button`
+  padding: 10px 20px;
+  font-size: 16px;
+  color: white;
+  background-color: #007bff;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  transition: background-color 0.3s;
+
+  &:hover {
+    background-color: #0056b3;
+  }
+`;

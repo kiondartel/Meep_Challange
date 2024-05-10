951de16f49635bb11136b3981727b1df6e7b8b64
Gabriel Lucas <gabriellucasseverino@hotmail.com>
2024-05-09 23:17:05 -0300

    Feat: add Product Module with product fetching

---
+++ src/modules/Products/index.tsx
@@ -0,0 +1,32 @@
+import { FilterByStatusContainer, Container } from "./styles";
+import React from "react";
+import { useQuery } from "react-query";
+
+import { Product } from "../../service/products/payload/productPayload";
+import { fetchProducts } from "../../service/products/ProductService";
+
+const ProductShow: React.FC = () => {
+  const pageToLoad = 1;
+  const rowsPerPage = 10;
+  const sortBy = "name";
+  const orderBy = "ASC";
+  const {
+    data: products,
+    error,
+    isLoading,
+  } = useQuery<Product[], Error>(
+    ["products", pageToLoad, rowsPerPage, sortBy, orderBy],
+    () => fetchProducts(pageToLoad, rowsPerPage, sortBy, orderBy),
+    {
+      retry: true,
+    }
+  );
+
+  return (
+    <Container>
+      <FilterByStatusContainer></FilterByStatusContainer>
+    </Container>
+  );
+};
+
+export default ProductShow;

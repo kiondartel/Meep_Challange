a2c993072f6c92f77e997918402398c76e837881
Gabriel Lucas <gabriellucasseverino@hotmail.com>
2024-05-10 08:18:03 -0300

    feat: add cartOverlay with purchased items

--- src/components/ShoppingCart/index.tsx
+++ src/components/ShoppingCart/index.tsx
@@ -1,24 +1,36 @@
 import React, { useState } from "react";
 import { FaShoppingCart } from "react-icons/fa";
 import { ShoppingCartWrapper, CartItemCount } from "./styles";
+import { Product } from "../../service/products/payload/productPayload";
+import CartOverlayComponent from "../CartOverlay";
 
 interface ShoppingCartProps {
   total: number;
+  itens: Product[];
 }
 
-const ShoppingCart: React.FC<ShoppingCartProps> = ({ total }) => {
+const ShoppingCart: React.FC<ShoppingCartProps> = ({ total, itens }) => {
   const [isCartOpen, setCartOpen] = useState<boolean>(false);
+  const totalPrice = itens.reduce((sum, item) => sum + Number(item.price), 0);
 
   return (
-    <ShoppingCartWrapper
-      data-testid="shopping-cart-wrapper"
-      onClick={() => setCartOpen(true)}
-    >
-      <FaShoppingCart size={22} />
-      <CartItemCount>
-        <strong>{total} </strong>
-      </CartItemCount>
-    </ShoppingCartWrapper>
+    <>
+      <ShoppingCartWrapper
+        data-testid="shopping-cart-wrapper"
+        onClick={() => setCartOpen(true)}
+      >
+        <FaShoppingCart size={22} />
+        <CartItemCount>
+          <strong>{total} </strong>
+        </CartItemCount>
+      </ShoppingCartWrapper>
+      <CartOverlayComponent
+        isOpen={isCartOpen}
+        setCartOpen={setCartOpen}
+        itens={itens}
+        totalPrice={totalPrice}
+      />
+    </>
   );
 };
 export default ShoppingCart;

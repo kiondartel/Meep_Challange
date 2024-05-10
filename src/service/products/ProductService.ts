b39e714e5ee70f315f273537cce4bc57dddafdd3
Gabriel Lucas <gabriellucasseverino@hotmail.com>
2024-05-09 23:11:18 -0300

    Feat: add product service with api call and product type

---
+++ src/service/products/ProductService.ts
@@ -0,0 +1,20 @@
+import { Product } from "./payload/productPayload";
+
+const BASE_URL = process.env.REACT_APP_BASE_URL;
+
+// Peguei uma api publica que retorna alguns itens eletronicos
+export const fetchProducts = async (
+  page: number,
+  rows: number,
+  sortBy: string,
+  orderBy: "ASC" | "DESC"
+): Promise<Product[]> => {
+  const response = await fetch(
+    `${BASE_URL}/products?page=${page}&rows=${rows}&sortBy=${sortBy}&orderBy=${orderBy}`
+  );
+  if (!response.ok) {
+    throw new Error("Network response was not ok");
+  }
+  const data = await response.json();
+  return data.products;
+};

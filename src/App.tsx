03038686f2130d38238fddcd06ceda76683f99ab
Gabriel Lucas <gabriellucasseverino@hotmail.com>
2024-05-09 23:32:10 -0300

    fix: add QueryClientProvider to enable React Query usage

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import store from "./store";
 import { Provider } from "react-redux";
 import RoutesConfig from "./routes";
-
+import { QueryClient, QueryClientProvider } from "react-query";
+const queryClient = new QueryClient();
 const App = () => {
   return (
     <Provider store={store}>
-      <RoutesConfig />
+      <QueryClientProvider client={queryClient}>
+        <RoutesConfig />
+      </QueryClientProvider>
     </Provider>
   );
 };
